e83d0323580588b2aa3d5a9326e700400e799842
zaoyu63 <zaoyu63@163.com>
2019-03-01 15:57:33 +0800

    去掉eslint警告, 调整地图

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,10 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-control-regex': 0,
+    'no-unused-vars': 0,
+    'no-useless-escape': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
